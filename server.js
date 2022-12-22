34cb38d0deac8b2c0f539b737934219d8bb798e8
Cristopher <criizec@gmail.com>
2022-12-21 19:49:23 -0600

    temperatura

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 // Librería para consumir urls
 import fetch from "node-fetch";
-import express from "express";
+import express, { request } from "express";
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 const host = 'localhost';
 const port = 8080;
 
 app.listen(port, () => {
-    console.log(`Servidor acttivo en el puerto ${port}`);
+    console.log(`Servidor activo en el puerto ${port}`);
 });
 
 app.use(cors({
@@ -59,4 +61,13 @@ app.get('/magnitudes', async function(req, res){
     const response = await fetch("https://eonet.gsfc.nasa.gov/api/v3/magnitudes");
     const data  = await response.json();
     res.send(data);
+});
+app.post('/temperatura', async function(req,res){
+    let coordenadas= "http://api.weatherstack.com/current?access_key=540219d6872b08bbbdad73466a7cd114&query="+req.body.lat+","+req.body.lon;
+    console.log(coordenadas);
+    const response = await fetch(coordenadas);
+    const data  = await response.json();
+    console.log(data)
+    res.send(data);
+    
 });
